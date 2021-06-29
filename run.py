e04c23eb27b1a9faae20912ceaf1a56eb01ca619
andrewskyboss <andrew.cybossky@gmail.com>
2021-06-29 22:56:03 +0000

    Adjust validation

--- run.py
+++ run.py
@@ -39,8 +39,9 @@ def validate_data(values):
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
-    """
+    """ 
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError( f"Exactly 6 values required, you provided {len(values)}")
     except ValueError as e:
@@ -48,5 +49,3 @@ def validate_data(values):
 
 
 get_sales_data()
-
-
